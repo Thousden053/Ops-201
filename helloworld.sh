279a9fd4f5f9736c0a81779ff7385d6d97eae479
tph <thousden053@outlook.com>
2023-02-08 05:20:39 +0000

    adds helloworld.sh file

---
+++ helloworld.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 02 Ops Challenge Solution
+# Author: Tyler Housden
+# Date of latest revision: 02/07/2023
+# Purpose: Prints a string to the terminal
+
+# Main
+printf  "I am working perfectly\n"
+
+
+
+
+# End
